09e0cafa37036a7fc4152b9ee5cc9b670b2e129a
LBarnes9 <lanesbarnes@gmail.com>
2026-06-08 14:59:19 -0600

    add data to mapbox as geoJSON sources

--- src/App.tsx
+++ src/App.tsx
@@ -1,226 +1,318 @@
-import { useRef, useEffect, useState } from 'react'
-import mapboxgl from 'mapbox-gl'
-import { SearchBox } from '@mapbox/search-js-react'
-import fireSVG from './assets/fire.svg'
-import planeSVG from './assets/airplane.svg'
-import { renderToString } from 'react-dom/server';
+import { useRef, useEffect, useState } from "react";
+import mapboxgl, { GeoJSONSource } from "mapbox-gl";
+import { SearchBox } from "@mapbox/search-js-react";
+import fireIcon from "./assets/fire.webp";
+import fireGreyIcon from "./assets/fireGrey.webp";
+import planeIcon from "./assets/plane.png";
+import helicopterIcon from "./assets/helicopter.png";
+import type { FeatureCollection } from "geojson";
+import { renderToString } from "react-dom/server";
 
-import 'mapbox-gl/dist/mapbox-gl.css'
-import './App.css'
+import "mapbox-gl/dist/mapbox-gl.css";
+import "./App.css";
 
-const accessToken = import.meta.env.VITE_MAPBOX_ACCESS_TOKEN
-const center:[number, number] = [-104.9888567141373, 39.73923582717103];
+const accessToken = import.meta.env.VITE_MAPBOX_ACCESS_TOKEN;
+const center: [number, number] = [-104.9888567141373, 39.73923582717103];
 
 function App() {
-  const mapRef = useRef<mapboxgl.Map | undefined>(undefined)
-  const mapContainerRef = useRef<HTMLDivElement>(null)
-  const [inputValue, setInputValue] = useState('')
+  const mapRef = useRef<mapboxgl.Map | undefined>(undefined);
+  const mapContainerRef = useRef<HTMLDivElement>(null);
+  const [inputValue, setInputValue] = useState("");
 
-  const fireMarkers: mapboxgl.Marker[] = []
-  const aircraftMarkers: mapboxgl.Marker[] = []
+  const fireSourceId = "fires-data-source";
+  const aircraftSourceId = "aircraft-data-source";
 
   useEffect(() => {
     mapRef.current = new mapboxgl.Map({
       accessToken,
-      style: 'mapbox://styles/mapbox/standard',
+      style: "mapbox://styles/mapbox/standard",
       container: mapContainerRef.current!,
       center,
       zoom: 10,
-    })
+    });
 
-    getFires().then((fireData: any[] | undefined) => {
-      if (fireData) {
-        const bounds = new mapboxgl.LngLatBounds();
+    const map = mapRef.current
+    if (map) {
+      map.on("load", async () => {
+        map.addSource(fireSourceId, {
+          type: "geojson",
+          data: "/api/fire/GeoJsonList?inactive=true",
+        });
 
-        fireData.forEach((fire: any) => {
-          const fireIcon = document.createElement('img');
-          fireIcon.className = 'fireIcon'
-          fireIcon.src = fireSVG;
-          fireIcon.width = 40;
-          fireIcon.height = 40;
-          if (!fire.IsActive) {
-            fireIcon.style.filter = 'grayscale(100%)';
+        const loadMapImage = (url: string, id: string) =>
+          new Promise<void>((resolve, reject) => {
+            map.loadImage(url, (error, image) => {
+              if (error) reject(error);
+              else {
+                map.addImage(id, image!);
+                resolve();
+              }
+            });
+          });
+
+        await Promise.all([
+          loadMapImage(fireIcon, "fireIcon"),
+          loadMapImage(fireGreyIcon, "fireGreyIcon"),
+          loadMapImage(planeIcon, "planeIcon"),
+          loadMapImage(helicopterIcon, "helicopterIcon"),
+        ]);
+
+        map.addLayer({
+          id: "fires-layer",
+          type: "symbol",
+          source: fireSourceId,
+          layout: {
+            // If a fire is not active, make it grey
+            "icon-image": [
+              "case",
+              ["get", "IsActive"],
+              "fireIcon",
+              "fireGreyIcon",
+            ],
+            "icon-size": 1,
+            "icon-allow-overlap": true,
+            "symbol-sort-key": ["to-number", ["get", "IsActive"]], // Active fires appear on top
+          },
+        });
+
+        getAircraft().then((aircraftData: FeatureCollection | undefined) => {
+          if (aircraftData) {
+            map.addSource(aircraftSourceId, {
+              type: "geojson",
+              data: aircraftData,
+            });
+
+            map.addLayer({
+              id: "aircraft-layer",
+              type: "symbol",
+              source: aircraftSourceId,
+              layout: {
+                "icon-image": [
+                  "match",
+                  ["get", "aircraftFunction"],
+                  "Helicopter",
+                  "helicopterIcon",
+                  "planeIcon",
+                ],
+                "icon-size": 1,
+                "icon-allow-overlap": true,
+                "icon-rotate": ["get", "heading"],
+              },
+            });
           }
-  
-          const fireMarker = new mapboxgl.Marker(fireIcon)
-            .setLngLat([fire.Longitude, fire.Latitude])
-            .setPopup(new mapboxgl.Popup().setHTML(renderToString(fireInfoView(fire))))
-            .addTo(mapRef.current!);
-  
-          fireMarkers.push(fireMarker)
-  
-          bounds.extend([fire.Longitude, fire.Latitude]);
         });
-  
-        mapRef.current!.fitBounds(bounds, {
-          padding: 100
-        });
-      }
-    });
 
-    setInterval(() => {
-      getAircraft().then((aircraftData: any[] | undefined) => {
-        if (aircraftData) {
-          aircraftMarkers.forEach((marker) => {
-            marker.remove()
-          })
-  
-          aircraftData.forEach((aircraft) => {
-            if (aircraft.attributes) {
-  
+        setInterval(() => {
+          getAircraft().then((aircraftData: FeatureCollection | undefined) => {
+            if (aircraftData) {
+              const source = map.getSource(
+                aircraftSourceId,
+              ) as GeoJSONSource;
+              source.setData(aircraftData);
             }
-            const aircraftIcon = document.createElement('img');
-            aircraftIcon.className = 'aircraftIcon'
-            aircraftIcon.src = planeSVG;
-            aircraftIcon.width = 40;
-            aircraftIcon.height = 40;
-    
-            const aircraftMarker = new mapboxgl.Marker(aircraftIcon)
-              .setLngLat([aircraft.geometry.x, aircraft.geometry.y])
-              .setRotation(aircraft.attributes.heading)
-              .setPopup(new mapboxgl.Popup().setHTML(renderToString(aircraftInfoView(aircraft))))
-              .addTo(mapRef.current!);
-  
-            aircraftMarkers.push(aircraftMarker)
-          })
-        }
-      })
-  }, 5000);
+          });
+        }, 5000);
 
-    mapRef.current.on('click', (e) => {
-      console.log('Click at:', e.lngLat);
-    });
+        map.fitBounds(
+          [
+            [-124.88, 32.39],
+            [-114.05, 41.97],
+          ],
+          {
+            padding: 50,
+            animate: true,
+          },
+        );
+      });
+
+      map.on("click", "fires-layer", (e) => {
+        const features = map.queryRenderedFeatures(e.point, {
+          layers: ["fires-layer"],
+        });
+
+        console.log("features[0]", features[0]);
+
+        let coordinates: GeoJSON.Position = [0, 0];
+        if (features.length > 0) {
+          coordinates = (features[0].geometry as GeoJSON.Point).coordinates;
+          console.log("Symbol Coordinates:", coordinates);
+        }
+
+        new mapboxgl.Popup()
+          .setLngLat([coordinates[0], coordinates[1]])
+          .setHTML(renderToString(fireInfoView(features[0])))
+          .addTo(map);
+      });
+
+      map.on("click", "aircraft-layer", (e) => {
+        const features = map.queryRenderedFeatures(e.point, {
+          layers: ["aircraft-layer"],
+        });
+
+        console.log("features[0]", features[0]);
+
+        let coordinates: GeoJSON.Position = [0, 0];
+        if (features.length > 0) {
+          coordinates = (features[0].geometry as GeoJSON.Point).coordinates;
+          console.log("Symbol Coordinates:", coordinates);
+        }
+
+        new mapboxgl.Popup()
+          .setLngLat([coordinates[0], coordinates[1]])
+          .setHTML(renderToString(aircraftInfoView(features[0])))
+          .addTo(map);
+      });
+
+      map.on("click", (e) => {
+        console.log("Click at:", e.lngLat);
+      });
+    }
 
     return () => {
-      mapRef.current?.remove()
-    }
-  }, [])
+      mapRef.current?.remove();
+    };
+  }, []);
 
   return (
     <>
-        <div style={{
-            margin: '10px 10px 0 0',
-            width: 300,
-            right: 0,
-            top: 0,
-            position: 'absolute',
-            zIndex: 10 }}>
-            <SearchBox 
-                accessToken={accessToken}
-                map={mapRef.current}
-                mapboxgl={mapboxgl}
-                value={inputValue}
-                options={{
-                  proximity: center
-                }}
-                onChange={(d) => {
-                    setInputValue(d);
-                }}
-                marker
-            />
-        </div>
-        <div id='map-container' ref={mapContainerRef} />
+      <div
+        style={{
+          margin: "10px 10px 0 0",
+          width: 300,
+          right: 0,
+          top: 0,
+          position: "absolute",
+          zIndex: 10,
+        }}
+      >
+        <SearchBox
+          accessToken={accessToken}
+          map={mapRef.current}
+          mapboxgl={mapboxgl}
+          value={inputValue}
+          options={{
+            proximity: center,
+          }}
+          onChange={(d) => {
+            setInputValue(d);
+          }}
+          marker
+        />
+      </div>
+      <div id="map-container" ref={mapContainerRef} />
     </>
-  )
+  );
 }
 
-const getFires = async (): Promise<any[] | undefined> => {
+const getAircraft = async (): Promise<FeatureCollection | undefined> => {
   try {
-    const response = await fetch('/api/fire/List?inactive=true'); // use GeoJsonList?
-    
+    const response = await fetch("/api/aircraft");
+
     if (!response.ok) {
       throw new Error(`HTTP error! status: ${response.status}`);
     }
-    
-    const fireData = await response.json();
-    // console.log("fireData", fireData);
-    return fireData
-  } catch (error) {
-    console.error('There was an error:', error);
-  }
-}
 
-const getAircraft = async (): Promise<any[] | undefined> => {
-  try {
-    const response = await fetch('/api/aircraft');
-    
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
-    }
-    
     const aircraftData = await response.json();
-    // console.log("aircraftData", aircraftData);
-    return aircraftData
+    console.log("aircraftData", aircraftData);
+
+    // Transform aircraft data into geoJSON
+    const geoJsonAircraftData: FeatureCollection = {
+      type: "FeatureCollection",
+      features: aircraftData.map((aircraft: any) => {
+        return {
+          type: "Feature",
+          geometry: {
+            type: "Point",
+            coordinates: [aircraft.geometry.x, aircraft.geometry.y],
+          },
+          properties: aircraft.attributes,
+        };
+      }),
+    };
+
+    console.log("geoJsonAircraftData", geoJsonAircraftData);
+    return geoJsonAircraftData;
   } catch (error) {
-    console.error('There was an error:', error);
+    console.error("There was an error:", error);
   }
-}
+};
 
 const fireInfoView = (fire: any) => {
   return (
     <>
-      <h1>{fire.Name}</h1>
-      {fire.IsActive ? 
-        <h3>{'Status: Active'}</h3> :
-        <h3>{`Status: Extinguished on ${fire.ExtinguishedDateOnly}`}</h3>}
+      <h1>{fire.properties.Name}</h1>
+      {fire.properties.IsActive ? (
+        <h3>{"Status: Active"}</h3>
+      ) : (
+        <h3>{`Status: Extinguished on ${fire.properties.ExtinguishedDateOnly}`}</h3>
+      )}
       <table>
         <tbody>
           <tr>
             <th>Start Date</th>
-            <td>{fire.StartedDateOnly}</td>
+            <td>{fire.properties.StartedDateOnly}</td>
           </tr>
           <tr>
             <th>Last Updated</th>
-            <td>{fire.Updated}</td>
+            <td>{fire.properties.Updated}</td>
           </tr>
           <tr>
             <th>Admin Unit</th>
-            <td>{fire.AdminUnit}</td>
+            <td>{fire.properties.AdminUnit}</td>
           </tr>
           <tr>
             <th>County</th>
-            <td>{fire.County}</td>
+            <td>{fire.properties.County}</td>
           </tr>
           <tr>
             <th>Location</th>
-            <td>{fire.Location}</td>
+            <td>{fire.properties.Location}</td>
           </tr>
           <tr>
             <th>Acres Burned</th>
-            <td>{fire.AcresBurned}</td>
+            <td>{fire.properties.AcresBurned}</td>
           </tr>
           <tr>
             <th>Percent Contained</th>
-            <td>{fire.PercentContained}</td>
+            <td>{fire.properties.PercentContained}</td>
           </tr>
         </tbody>
       </table>
     </>
-  )
-}
+  );
+};
 
 const aircraftInfoView = (aircraft: any) => {
   return (
     <>
-      <h1>{aircraft.attributes.ident}</h1>
+      <h1>{`Identifier: ${aircraft.properties.ident}`}</h1>
+      <h2>{`Registration: ${aircraft.properties.reg}`}</h2>
       <table>
         <tbody>
           <tr>
+            <th>Function</th>
+            <td>{`${aircraft.properties.aircraftFunction}`}</td>
+          </tr>
+          <tr>
             <th>Heading</th>
-            <td>{`${aircraft.attributes.heading} deg`}</td>
+            <td>{`${aircraft.properties.heading} deg`}</td>
           </tr>
           <tr>
             <th>Altitude</th>
-            <td>{aircraft.attributes.alt}</td>
+            <td>{aircraft.properties.alt}</td>
+          </tr>
+          <tr>
+            <th>Ground Speed</th>
+            <td>{aircraft.properties.gs}</td>
           </tr>
           <tr>
             <th>Owner</th>
-            <td>{aircraft.attributes.owner}</td>
+            <td>{aircraft.properties.owner}</td>
           </tr>
         </tbody>
       </table>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
